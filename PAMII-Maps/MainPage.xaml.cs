34485f0cbff455653d8fb85df4d92b5e7fbab678
ana e carlos <anatevezvitoriarosseto@gmail.com>
2024-09-18 11:37:13 -0300

    Aula Mapas - API Google

---
+++ PAMII-Maps/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Maui.Maps;
+
+namespace PAMII_Maps
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //Inicialização do Mapa
+            var location = new Location(-23.51526648924293, -46.58024204632859);
+            var mapSpan = new MapSpan(location, 0.01, 0.01);
+            map.MoveToRegion(mapSpan);
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+        
+        }
+    }
+
+}
